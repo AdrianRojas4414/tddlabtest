9fd6adea3feef1f13c8fb522491b82983cc19fce
adrian <adrian.rojas.o@gmail.com>
2025-09-29 21:07:26 -0400

    pruebas pasan

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -5,25 +5,5 @@ describe("Sumar", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
 
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
-  });
-
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
-  });
-
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(4);
-  });
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(4);
-  });
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(4);
-  });
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(4);
-  });
 });
 
